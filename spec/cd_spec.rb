ce5103d0bc49cebe8abefdb73c0381893b5dc2de
Duane M. Moody <dmmoody@gmail.com>
2015-01-15 12:03:14 -0800

    add #all spec

--- spec/cd_spec.rb
+++ spec/cd_spec.rb
@@ -23,6 +23,16 @@ describe CD do
       expect(new_cd.add()).to(eq({new_cd.album_name() => new_cd.artist()}))
     end
   end
+
+  describe('#all') do
+    it('will show all CDs in cd_collection with album name and artist') do
+      new_cd1 = CD.new("Back in Black", "ACDC")
+      new_cd1.add()
+      new_cd2 = CD.new('Volcano', 'Jimmy Buffet')
+      new_cd2.add()
+      expect(CD.all()).to(eq({"Back in Black" => "ACDC", "Volcano" => "Jimmy Buffet"}))
+    end
+  end
 end
 
 
