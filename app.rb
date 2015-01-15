a25b4456d0cfa070329df287ba417f8a2f4a3bc8
Duane M. Moody <dmmoody@gmail.com>
2015-01-15 14:34:33 -0800

    Add 'list all CDs' and 'new CD add' functionality.

--- app.rb
+++ app.rb
@@ -4,5 +4,14 @@ require("./lib/cd")
 also_reload("./lib/**/*.rb")
 
 get("/") do
+  @cd = CD.all()
   erb(:index)
+end
+
+post('/new_cd_entry') do
+  @album_name = params.fetch('album_name')
+  @artist = params.fetch('artist')
+  @new_cd = CD.new(@album_name, @artist)
+  @new_cd.add()
+  redirect('/')
 end
